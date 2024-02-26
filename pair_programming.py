ab0974fe68182debfd6da9f51794f5a39561bd3c
Julia Berglind <jberglind@mines.edu>
2024-02-26 12:11:31 -0700

    Add feedback and test function

--- pair_programming.py
+++ pair_programming.py
@@ -1,3 +1,4 @@
+# Julia's comments: Function doc string looks great, very detailed and explains what the function does along with an example. You could maybe shorten the description a little shorter but it's not necessary c:
 def polar_to_cart(r, theta):
     """Converts coordinates from polar to cartesian.
     
@@ -26,9 +27,10 @@ def polar_to_cart(r, theta):
     >>> polar_to_cart(2, math.pi)
     [-2.0, 2.4492935982947064e-16]
     """
-    
+
+    # Julia's comments: documentation looks great! Code is very clear and you explain what you are doing and why very well. The use of white space is also very nice. No changes needed here!
     # import math module
-    import math
+    import math # Julia's comments: math needs to be imported outside of the function
     
     # calculate x and y
     x = r * math.cos(theta)
@@ -38,3 +40,19 @@ def polar_to_cart(r, theta):
     cartesian_pt = [x, y]
     
     return cartesian_pt
+
+# Julia's test function:
+def test_polar_to_cart():
+    # test case 1
+    result1 = polar_to_cart(4, math.pi/3)
+    assert result1 == [2.0000000000000004, 3.4641016151377544]
+    
+    # test case 2
+    result2 = polar_to_cart(2, math.pi)
+    assert result2 == [-2.0, 2.4492935982947064e-16]
+    
+    print("All tests passed.")
+
+# call function
+test_polar_to_cart()
+    
